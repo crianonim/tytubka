5995d72f641ddb78d110369caa5e7b1a35c30119
Jan Skowronski <crianonim@gmail.com>
2019-08-04 12:13:56 +0100

    Starting integrating of vue, initial Google Element

---
+++ client/src/components/GoogleSignIn.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+    <a href="#" onclick="signOut();">Sign out</a>
+  </div>
+</template>
+
+<script>
+    
+export default {
+  data(){
+      return {
+
+      }
+  },
+  methods: {
+      
+
+  },
+  mounted(){
+      console.log("Mounted");
+      console.log(window.guser);
+      console.log(window.gauth);
+      window.guserProfileReady=function(profile){
+          console.log("GOT",profile);
+      }
+  }
+};
+
+</script>
+
+<style>
+</style>
