f6f47313b305ec637e780c3ee092b7422b2fc303
Jan Skowronski <crianonim@gmail.com>
2019-08-04 16:14:48 +0100

    Basic styling of GoogleSignIn component

--- client/src/components/GoogleSignIn.vue
+++ client/src/components/GoogleSignIn.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>
+  <div class="flex-container">
     <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
-    <a href="#" onclick="signOut();">Sign out</a>
-    <p>{{profile}}</p>
+    <p v-if="profile">{{profile.ig}}</p>
+    <img v-if="profile" class="avatar" :src="profile.Paa">
+    <a v-if="profile" class="abcRioButtonBlue signout" href="#" onclick="signOut();">Sign out</a>
   </div>
 </template>
 
@@ -40,4 +41,24 @@ export default {
 </script>
 
 <style>
+.g-signin2 {
+  margin:0 1rem;
+}
+.flex-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem 0;
+}
+.signout {
+  font-size: 13px;
+  flex-grow: 0;
+  margin:0 1rem;
+  padding: 10px;
+}
+img.avatar {
+width: 2rem;
+border-radius: 1rem;
+margin: 0 1rem;
+}
 </style>
