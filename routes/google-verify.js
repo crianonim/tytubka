e622e59da68b2a205784c118a3de4463e39e8066
Jan Skowronski <crianonim@gmail.com>
2019-08-05 08:33:45 +0100

    Avoid authorisation in public path for /info

--- routes/google-verify.js
+++ routes/google-verify.js
@@ -4,7 +4,10 @@ const {
 const client = new OAuth2Client();
 async function verify(req, res, next) {
   const token = req.query.id_token;
-  console.log("in middle", token)
+  console.log(req.url,"in middle", token);
+  if (req.url.startsWith('/info')||req.url.startsWith('/direct')){
+    next();
+  } else
   if (token && token!=="undefined") {
     console.log("Has token",token,typeof token)
     const ticket = await client.verifyIdToken({
@@ -16,8 +19,8 @@ async function verify(req, res, next) {
     req.user = payload;
     next();
   } else {
-    next();
-    // res.send();
+    // next();
+    res.send();
   }
 }
 
